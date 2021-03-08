65a270dc814af5aaf1d1be3fcdddffba4c6a727d
Tkacz <gabrielmt2@al.insper.edu.br>
2021-03-07 22:22:43 -0300

    att

--- ro3.py
+++ ro3.py
@@ -6,8 +6,16 @@ def rule_of_three(x1, x2, y1):
     return (x2*y1)/x1
 
 window = Tk()
+window.title("Rule of three calculator")
 
 n=[5, 5]
+
+blankColumn1=Label(window, text="           ")
+blankColumn2=Label(window, text="           ")
+blankRow1=Label(window, text="           ")
+blankRow2=Label(window, text="           ")
+blankSpace1=Label(window, text="           ")
+blankSpace2=Label(window, text="           ")
 
 x1=Entry(window)
 x2=Entry(window)
@@ -32,10 +40,16 @@ isToLower=Label(window, text="is to")
 # for column in range(n[1]):
 #     Label(window, text="           ").grid(row=0, column=column)
 
+blankColumn1.grid(row=0, column=0, rowspan=5)
+blankColumn2.grid(row=0, column=5, rowspan=5)
+blankRow1.grid(row=0, column=1, columnspan=3)
+blankRow2.grid(row=5, column=1, columnspan=3)
+#blankSpace1.grid(row=2, column=2)
+#blankSpace2.grid(row=2, column=2)
 
-x1.grid(row=2, column=2)
-x2.grid(row=2, column=4)
-y1.grid(row=4, column=2)
+x1.grid(row=2, column=2, padx=25, pady=10)
+x2.grid(row=2, column=4, padx=25, pady=10)
+y1.grid(row=4, column=2, padx=25, pady=10)
 #y2.grid(row=4, column=4)
 
 isToUpper.grid(row=2, column=3)
